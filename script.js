475ca4736b01f7fc8186cc05b019bd16e775bc83
Andres Lora <jalc.andres.lora@gmail.com>
2023-06-01 09:03:40 -0700

    implemented picture in picture when button is clicked

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('testing');
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element, then play it
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error here
+    console.log("whoops, error here:", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // Disable Button
+  button.disabled = true;
+
+  // Start Picture in Picture mode
+  await videoElement.requestPictureInPicture();
+
+  // Reset Button
+  button.disabled = false;
+});
+
+// On load
+selectMediaStream();
